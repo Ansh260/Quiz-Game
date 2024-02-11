389516936bbc9100219ef8555fd4dd3242ba481e
AnvitT <99025362+AnvitT@users.noreply.github.com>
2024-02-11 13:51:39 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,271 +1,170 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
 
-int login()
-{
-    char username[] = "admin";
-    char password[] = "123";
+#define QUESTIONS_FILE "questions.csv"
 
-    char inputUsername[100];
-    char inputPassword[100];
+#define MAX_QUESTIONS 25   // Change this if more questions are added.
+#define MAX_CATEGORY_LENGTH 200
+#define MAX_QUESTION_LENGTH 1000
+#define MAX_OPTION_LENGTH 200
+#define MAX_LINE_LENGTH 1024
+#define MAX_CATEGORIES 5   // Change this if another category is added. Also include the category in the fieldOfInterest array.
 
-    printf("Hello!\n");
-    printf("Enter your Username: ");
-    scanf("%s", &inputUsername);
 
-    printf("Enter your Password: ");
-    scanf("%s", &inputPassword);
+typedef struct Question {
+    char category[MAX_CATEGORY_LENGTH];
+    int id;
+    char question[MAX_QUESTION_LENGTH];
+    char option1[MAX_OPTION_LENGTH];
+    char option2[MAX_OPTION_LENGTH];
+    char option3[MAX_OPTION_LENGTH];
+    char option4[MAX_OPTION_LENGTH];
+    int answer;
+} question;
 
-    if (!strcmp(username, inputUsername) && !strcmp(password, inputPassword))
-    {
-        printf("Login Succesful\n\n");
-        return 1;
-    }
-    printf("Login Failed!\n");
+question Questions[MAX_QUESTIONS];
+char fieldOfInterest[MAX_CATEGORIES][MAX_CATEGORY_LENGTH] = {"Sports", "Entertainment", "Spirituality", "Technology", "Finance"};
+
+void loadQuestions();
+void selectCategory();
+void displayQuestions(char *selectedCategory);
+void clearScreen();
+void delay(int number_of_seconds);
+
+ int main() {
+    loadQuestions();
+    selectCategory();
     return 0;
 }
 
-int questions()
-{
+void loadQuestions() {
+    FILE *file = fopen(QUESTIONS_FILE, "r");
+    if (file == NULL) {
+        printf("File could not be opened.\n");
+        return;
+    }
+    int line_count = 0;
+    char buffer[MAX_LINE_LENGTH];
 
-    int numberOfQuestions = 5;
+    fgets(buffer, MAX_LINE_LENGTH, file);
+    while (fgets(buffer, MAX_LINE_LENGTH, file) && line_count < MAX_QUESTIONS) {
+        buffer[strcspn(buffer, "\n")] = '\0';
+        char *category = strtok(buffer, ",");
+        char *id_str = strtok(NULL, ",");
+        char *questionText = strtok(NULL, ",");
+        char *option1 = strtok(NULL, ",");
+        char *option2 = strtok(NULL, ",");
+        char *option3 = strtok(NULL, ",");
+        char *option4 = strtok(NULL, ",");
+        char *answer_str = strtok(NULL, ",");
+        
+        int id = atoi(id_str);
+        int answer = atoi(answer_str);
+
+        strncpy(Questions[line_count].category, category, MAX_CATEGORY_LENGTH - 1);
+        Questions[line_count].category[MAX_CATEGORY_LENGTH - 1] = '\0';
+        strncpy(Questions[line_count].question, questionText, MAX_QUESTION_LENGTH - 1);
+        Questions[line_count].question[MAX_QUESTION_LENGTH - 1] = '\0';
+        strncpy(Questions[line_count].option1, option1, MAX_OPTION_LENGTH - 1);
+        Questions[line_count].option1[MAX_OPTION_LENGTH - 1] = '\0';
+        strncpy(Questions[line_count].option2, option2, MAX_OPTION_LENGTH - 1);
+        Questions[line_count].option2[MAX_OPTION_LENGTH - 1] = '\0';
+        strncpy(Questions[line_count].option3, option3, MAX_OPTION_LENGTH - 1);
+        Questions[line_count].option3[MAX_OPTION_LENGTH - 1] = '\0';
+        strncpy(Questions[line_count].option4, option4, MAX_OPTION_LENGTH - 1);
+        Questions[line_count].option4[MAX_OPTION_LENGTH - 1] = '\0';
+
+        Questions[line_count].id = id;
+        Questions[line_count].answer = answer;
+
+        line_count++;
+    }
+    fclose(file);
+}
+
+void selectCategory() {
+    printf("Select your Field Of Interest: \n");
+    for (int i = 0; i < MAX_CATEGORIES; i++) {
+        printf("%d->%s\n", i + 1, fieldOfInterest[i]);
+    }
+    printf("\n");
+
+    int choice;
+    printf("Enter your choice (1-5): ");
+    if (scanf("%d", &choice) != 1) {
+        printf("Invalid input.\n");
+        return;
+    }
+
+    if (choice < 1 || choice > MAX_CATEGORIES) {
+        printf("Invalid choice. Exiting.\n");
+        return;
+    }
+
+    printf("\nHello! Welcome to your quiz on the topic of %s!\n\n", fieldOfInterest[choice - 1]);
+    
+    delay(1000);
+    clearScreen();
+    printf("Starting in 3...");
+    delay(1000);
+    clearScreen();
+    printf("Starting in 2...");
+    delay(1000);
+    clearScreen();
+    printf("Starting in 1...");
+    delay(500);
+    clearScreen();
+    displayQuestions(fieldOfInterest[choice - 1]);
+}
+
+void displayQuestions(char selectedCategory[]) {
     int score = 0;
     int inputAnswer;
-
-    typedef struct question
-    {
-        int questionid;
-        char question[1000];
-        char option1[200];
-        char option2[200];
-        char option3[200];
-        char option4[200];
-        int answer;
-    } Quiz;
-
-    Quiz Sports[numberOfQuestions];
-    strcpy(Sports[0].question, " Best Player in the world");
-    strcpy(Sports[0].option1, "AB Devilliers");
-    strcpy(Sports[0].option2, "Virat");
-    strcpy(Sports[0].option3, "Sachin");
-    strcpy(Sports[0].option4, "Dravid");
-    Sports[0].answer = 2;
-
-    strcpy(Sports[1].question, " Most Popular Sport in the world");
-    strcpy(Sports[1].option1, "Football");
-    strcpy(Sports[1].option2, "Cricket");
-    strcpy(Sports[1].option3, "Basketball");
-    strcpy(Sports[1].option4, "Rugby");
-    Sports[1].answer = 1;
-
-    strcpy(Sports[2].question, "Who won the 10th Italian Open title in 2021?");
-    strcpy(Sports[2].option1, "Novack Djokovic");
-    strcpy(Sports[2].option2, "Rafael Nadal");
-    strcpy(Sports[2].option3, "Dominic Thiem");
-    strcpy(Sports[2].option4, "Stefanos Tsitsipas");
-    Sports[2].answer = 2;
-
-    strcpy(Sports[3].question, "In cricket which of the following fielding positions is behind the batsman?");
-    strcpy(Sports[3].option1, "Mid-wicket");
-    strcpy(Sports[3].option2, "First slip");
-    strcpy(Sports[3].option3, "Cover");
-    strcpy(Sports[3].option4, "Mid-off");
-    Sports[3].answer = 2;
-
-    strcpy(Sports[4].question, "Neeraj Chopra won India’s first-ever Olympic Gold Medal in which of the following sports?");
-    strcpy(Sports[4].option1, "400m Hurdle");
-    strcpy(Sports[4].option2, "Javelin throw");
-    strcpy(Sports[4].option3, "Long Jump");
-    strcpy(Sports[4].option4, "High Jump");
-    Sports[4].answer = 2;
-
-    Quiz Entertainment[numberOfQuestions];
-    strcpy(Entertainment[0].question, "Who among the followings has been awarded the best director award by the Dadasaheb Phalke International Film Festival Awards 2022?");
-    strcpy(Entertainment[0].option1, "Ken Ghosh");
-    strcpy(Entertainment[0].option2, "Vishal Bharadwarj");
-    strcpy(Entertainment[0].option3, "Anurag Kashyap");
-    strcpy(Entertainment[0].option4, "S.S.Raja Mauli");
-    Entertainment[0].answer = 2;
-
-    strcpy(Entertainment[1].question, "Which of the following has bagged the ‘Best International Film’ award at the 6th Hollywood Critics Association (HCA) Film Awards?");
-    strcpy(Entertainment[1].option1, "RRR");
-    strcpy(Entertainment[1].option2, "KGF Chapter 2");
-    strcpy(Entertainment[1].option3, "Pushpa:The Rise");
-    strcpy(Entertainment[1].option4, "Brahmastra");
-    Entertainment[1].answer = 2;
-
-    strcpy(Entertainment[2].question, "Which album has won the 'Album of the Year' Grammy award 2022?");
-    strcpy(Entertainment[2].option1, "We Are");
-    strcpy(Entertainment[2].option2, "My Love");
-    strcpy(Entertainment[2].option3, "Help Yourself");
-    strcpy(Entertainment[2].option4, "This Time Tomorrow");
-    Entertainment[2].answer = 2;
-
-    strcpy(Entertainment[3].question, "Which of the following song has won the Best original song award at Oscar 2023?");
-    strcpy(Entertainment[3].option1, "Naacho Naacho");
-    strcpy(Entertainment[3].option2, "Yaara Teri Meri Yaari");
-    strcpy(Entertainment[3].option3, "Neendraan Ni Aandiyaa");
-    strcpy(Entertainment[3].option4, "Naatu Naatu");
-    Entertainment[3].answer = 2;
-
-    strcpy(Entertainment[4].question, "Who won the Best Playback Singer at the 2024 Filmfare Awards in the male and female categories?");
-    strcpy(Entertainment[4].option1, "Bhupinder Babbal and Shilpa Rao");
-    strcpy(Entertainment[4].option2, "Arjit Singh and Shilpa Rao");
-    strcpy(Entertainment[4].option3, "Sonu Nigam and Shreya Ghoshal");
-    strcpy(Entertainment[4].option4, "Bhupinder Babbal and Shreya Ghoshal");
-    Entertainment[4].answer = 2;
-
-    Quiz Spirituality[numberOfQuestions];
-    strcpy(Spirituality[0].question, "Bhagavad Gita was spoken. -years ago by Lord Krishna");
-    strcpy(Spirituality[0].option1, "5000");
-    strcpy(Spirituality[0].option2, "500");
-    strcpy(Spirituality[0].option3, "200");
-    strcpy(Spirituality[0].option4, "50000");
-    Spirituality[0].answer = 1;
-
-    strcpy(Spirituality[1].question, "There are verses in Bhagavad Gita:");
-    strcpy(Spirituality[1].option1, "7000");
-    strcpy(Spirituality[1].option2, "1800");
-    strcpy(Spirituality[1].option3, "700");
-    strcpy(Spirituality[1].option4, "108");
-    Spirituality[1].answer = 3;
-
-    strcpy(Spirituality[2].question, "Number of Chapters in Bhagavad Gita:");
-    strcpy(Spirituality[2].option1, "77");
-    strcpy(Spirituality[2].option2, "18");
-    strcpy(Spirituality[2].option3, "70");
-    strcpy(Spirituality[2].option4, "108");
-    Spirituality[2].answer = 2;
-
-    strcpy(Spirituality[3].question, "Bhagavad Gita was spoken at the battlefield of:");
-    strcpy(Spirituality[3].option1, "Hiroshima Nagasaki");
-    strcpy(Spirituality[3].option2, "Kurukshetra");
-    strcpy(Spirituality[3].option3, "Lanka");
-    strcpy(Spirituality[3].option4, "Vrindawan");
-    Spirituality[3].answer = 2;
-
-    strcpy(Spirituality[4].question, "Bhagavad Gita is also known as:");
-    strcpy(Spirituality[4].option1, "GitoPaveda");
-    strcpy(Spirituality[4].option2, "Upnisad Gita");
-    strcpy(Spirituality[4].option3, "Gitopanisad");
-    strcpy(Spirituality[4].option4, "Holy Book");
-    Spirituality[4].answer = 3;
-
-    Quiz Technology[numberOfQuestions];
-    strcpy(Technology[0].question, "What is part of a database that holds only one type of information?");
-    strcpy(Technology[0].option1, "Report");
-    strcpy(Technology[0].option2, "Field");
-    strcpy(Technology[0].option3, "Recond");
-    strcpy(Technology[0].option4, "File");
-    Technology[0].answer = 1;
-
-    strcpy(Technology[1].question, "Most modern TV's draw power even if turned off. The circuit the power is used in does what function?");
-    strcpy(Technology[1].option1, "Sound");
-    strcpy(Technology[1].option2, "Remote contro");
-    strcpy(Technology[1].option3, "Color balance");
-    strcpy(Technology[1].option4, "High voltage");
-    Technology[1].answer = 1;
-
-    strcpy(Technology[2].question, "Which is a type of Electrically-Erasable Programmable Read-Only Memory?");
-    strcpy(Technology[2].option1, "Flash");
-    strcpy(Technology[2].option2, "Flange");
-    strcpy(Technology[2].option3, "Fury");
-    strcpy(Technology[2].option4, "Fram");
-    Technology[2].answer = 1;
-
-    strcpy(Technology[3].question, "The most common format for a home video recorder is VHS. VHS stands for...?");
-    strcpy(Technology[3].option1, "Video Home System");
-    strcpy(Technology[3].option2, "Very high speed");
-    strcpy(Technology[3].option3, "Video horizontal standard");
-    strcpy(Technology[3].option4, "Voltage house standard");
-    Technology[3].answer = 1;
-
-    strcpy(Technology[4].question, "Programming a robot by physically moving it through the trajectory you want it to follow is called:");
-    strcpy(Technology[4].option1, "contact sensing control");
-    strcpy(Technology[4].option2, "continous-path control");
-    strcpy(Technology[4].option3, "robot vision control");
-    strcpy(Technology[4].option4, "pick-and-place control");
-    Technology[4].answer = 1;
-
-    Quiz Finance[numberOfQuestions];
-    strcpy(Finance[0].question, "Which of the following is not a direct tax?");
-    strcpy(Finance[0].option1, "Sales tax");
-    strcpy(Finance[0].option2, "Income tax");
-    strcpy(Finance[0].option3, "Wealth tax");
-    strcpy(Finance[0].option4, "Estate duty");
-    Finance[0].answer = 1;
-
-    strcpy(Finance[1].question, "The Indian economy is");
-    strcpy(Finance[1].option1, "capitalist");
-    strcpy(Finance[1].option2, "socialistic");
-    strcpy(Finance[1].option3, "Mixed");
-    strcpy(Finance[1].option4, "Federal");
-    Finance[1].answer = 1;
-
-    strcpy(Finance[2].question, "The economic liberalization was introduced in");
-    strcpy(Finance[2].option1, "1991");
-    strcpy(Finance[2].option2, "1990");
-    strcpy(Finance[2].option3, "1985");
-    strcpy(Finance[2].option4, "1988");
-    Finance[2].answer = 1;
-
-    strcpy(Finance[3].question, "The National Stock Exchange (NSE) is located at");
-    strcpy(Finance[3].option1, "Mumbai");
-    strcpy(Finance[3].option2, "New Delhi");
-    strcpy(Finance[3].option3, "Madras");
-    strcpy(Finance[3].option4, "Calcutta");
-    Finance[3].answer = 1;
-
-    strcpy(Finance[4].question, "Development means economic growth with");
-    strcpy(Finance[4].option1, "price stability");
-    strcpy(Finance[4].option2, "social change");
-    strcpy(Finance[4].option3, "inflation");
-    strcpy(Finance[4].option4, "deflation");
-    Finance[4].answer = 1;
-
-
-    for (int i = 0; i < numberOfQuestions; i++)
-    {
-
-        printf("Question %d : %s\n", i+1,Sports[i].question);
-        printf("Option 1 : %s\n", Sports[i].option1);
-        printf("Option 2 : %s\n", Sports[i].option2);
-        printf("Option 3 : %s\n", Sports[i].option3);
-        printf("Option 4 : %s\n \n", Sports[i].option4);
-        printf("Enter answer: ");
-        scanf("%d", &inputAnswer);
-        if (inputAnswer == Sports[i].answer)
-        {
-            printf("Correct Answer! :) \n \n");
-            score++;
-        }
-        else
-        {
-            printf("Incorrect Answer :( \n \n");
-            score--;
+    for (int i = 0; i < MAX_QUESTIONS; i++) {
+        if (strcmp(selectedCategory, Questions[i].category) == 0) {
+            printf("Question ID: %d", Questions[i].id);
+            printf("                                   Current Score: %d\n\n",score);
+            printf("Category: %s\n", Questions[i].category);
+            printf("Question: %s\n", Questions[i].question);
+            printf("Options:\n 1. %s\n 2. %s\n 3. %s\n 4. %s\n", Questions[i].option1, Questions[i].option2, Questions[i].option3, Questions[i].option4);
+            printf("Enter answer (1-4) or 0 to skip: ");
+            scanf("%d", &inputAnswer);
+            if (inputAnswer == Questions[i].answer) {
+                printf("Correct!\n\n");
+                score += 4;
+            }
+            else if (inputAnswer == 0) {
+                printf("Skipped.\n\n");
+                continue;
+            }
+            else {
+                printf("Incorrect :(\n\n");
+                score -= 1;
+            }
+            delay(700);
+            clearScreen();
         }
     }
-    printf("Your Scorecard: %d \n",score);
+    printf("Your Score: %d\n\n", score);
 }
 
-int main()
+void clearScreen()
 {
-    if (login())
-    {
-        printf("Select your Field Of Interest: \n");
-        int length = 5;
-        char fieldOfInterest[5][100] = {"Sports", "Entertainment", "Spirituality", "Technology", "Finance"};
-        for (int i = 0; i < length; i++)
-        {
-            printf("%d->%s\n", i + 1, fieldOfInterest[i]);
-        }printf("\n");
-        
-        int choice;
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-        printf("\n");
-        printf("Hello! Welcome to your quiz on the topic of %s!\n \n", fieldOfInterest[choice - 1]);
-        questions();
-    }
-    return 0;
+  const char *CLEAR_SCREEN_ANSI = "\x1b[1;1H\x1b[2J";
+  ssize_t bytes_written = write(STDOUT_FILENO, CLEAR_SCREEN_ANSI, strlen(CLEAR_SCREEN_ANSI));
+
+  if (bytes_written == -1) {
+    perror("Failed to clear screen");
+  }
+}
+
+void delay(int milli_seconds)
+{
+    struct timespec ts;
+    ts.tv_sec = milli_seconds / 1000;
+    ts.tv_nsec = (milli_seconds % 1000) * 1000000L;
+    nanosleep(&ts, NULL);
 }
